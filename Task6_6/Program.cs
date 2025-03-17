29e7a7fad127cd476c925024b4a59c8a10ccb667
Виолетта Солнцева <stepviol@live.com>
2025-03-17 09:34:14 +0300

    HW6

---
+++ Task6_6/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Task6_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Составить регулярное выражение для проверки корректности пароля.
+            // Пароль должен состоять минимум из 14 символов и иметь в составе
+            // минимум одну цифру,
+            // заглавную букву,
+            // строчную букву и
+            // специальный символ из набора!#;%:?*
+            // Запрашивать у пользователя пароль до тех пор, пока он не введет пароль, удовлетворяющий регулярному выражению.
+
+
+            Console.Write("Введите пароль (минимум 14 символов, содержит: цифру, заглавную букву, строчную букву и специальный символ из набора!#;%:?* : ");
+            string pass = Console.ReadLine();
+
+            int n = 0;
+            n = pass.Length;
+
+            while (n < 14)
+            {
+                Console.Write("Введите пароль не менее 14 символов: ");
+                pass = Console.ReadLine();
+            }
+
+            Regex regex1 = new Regex(@"[0-9]");
+            Regex regex2 = new Regex(@"[A-ZА-я]");
+            Regex regex3 = new Regex(@"[a-zа-я]");
+            Regex regex4 = new Regex(@"[!#;%:?*]");
+
+            foreach (char c in pass)
+            {
+                if (regex1.IsMatch(pass))
+                {
+                    if (regex2.IsMatch(pass))
+                    {
+                        if (regex3.IsMatch(pass))
+                        {
+                            if (regex4.IsMatch(pass))
+                            {
+                                Console.WriteLine("Пароль введен верно!");
+                                break;
+                            }
+                            else
+                            {
+                                Console.Write("Введите корректный пароль (пароль должен содержать хотя бы один символ из набора !#;%:?*.): ");
+                                pass = Console.ReadLine();
+                            }
+                        }
+                        else
+                        {
+                            Console.Write("Введите корректный пароль (пароль должен содержать хотя бы одну строчную букву): ");
+                            pass = Console.ReadLine();
+                        }
+                    }
+                    else
+                    {
+                        Console.Write("Введите корректный пароль (пароль должен содержать хотя бы одну заглавную букву): ");
+                        pass = Console.ReadLine();
+                    }
+                }
+                else
+                {
+                    Console.Write("Введите корректный пароль (пароль должен содержать хотя бы одну цифру): ");
+                    pass = Console.ReadLine();
+                }
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Task6_6a/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Task6_6a
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Регулярное выражение для проверки пароля
+            string passwordPattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!#;%:?*]).{14,}$";
+
+            // Создание объекта Regex
+            Regex regex = new Regex(passwordPattern);
+
+            string password;
+            do
+            {
+                // Запрос пароля у пользователя
+                Console.WriteLine("Введите пароль (минимум 14 символов, включая цифру, заглавную и строчную буквы, и специальный символ из набора !#;%:?*):");
+                password = Console.ReadLine();
+
+                // Проверка пароля с помощью регулярного выражения
+                if (!regex.IsMatch(password))
+                {
+                    Console.WriteLine("Пароль не соответствует требованиям. Попробуйте снова.");
+                }
+
+            } while (!regex.IsMatch(password));
+
+            Console.WriteLine("Пароль принят.");
+
+            Console.ReadKey();
+        }
+    }
+}
