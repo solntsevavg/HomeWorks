446770284bef6ff033d063d51c4740df88e89f57
Виолетта Солнцева <stepviol@live.com>
2025-03-24 09:44:14 +0300

    HW7

---
+++ Task7_1/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Task7_1
+{
+    internal class Program
+    {
+        // Два треугольника заданы длинами своих сторон. Определите, площадь какого из них больше
+        // (создайте метод Square для вычисления площади треугольника по длинам его сторон).
+        // Для решения задачи можно использовать формулу Герона.
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите стороны первого треугольника");
+            double a1 = Convert.ToDouble(Console.ReadLine());
+            double b1 = Convert.ToDouble(Console.ReadLine());
+            double c1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите стороны второго треугольника");
+            double a2 = Convert.ToDouble(Console.ReadLine());
+            double b2 = Convert.ToDouble(Console.ReadLine());
+            double c2 = Convert.ToDouble(Console.ReadLine());
+
+            double result1 = Square(a1, b1, c1);
+            double result2 = Square(a2, b2, c2);
+
+            Console.WriteLine($"{Math.Round(result1, 3)}  {Math.Round(result2, 3)}"); // для проверки
+
+            if (result1 == result2)
+                Console.WriteLine("Треугольники равны по площади");
+            else
+            {
+                if (result1 > result2)
+                    Console.WriteLine("Площадь первого треугольника больше");
+                else
+                    Console.WriteLine("Площадь второго треугольника больше");
+            }
+
+            Console.ReadLine();
+        }
+
+
+        // Вычисление площади по формуле Герона
+        static double Square(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+    }
+}
---
+++ Task7_4/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Task7_4
+{
+    internal class Program
+    {
+        // Напишите метод FindMax, который принимает переменное количество чисел
+        // и возвращает максимальное значение.Используйте ключевое слово params.
+
+        static void Main()
+        {
+            // Передача максимального числа
+
+            int maxNumber = FindMax(10, 25, 5, 30, 15);
+
+            Console.WriteLine($"Максимальное число: {maxNumber}");
+            Console.ReadKey();
+        }
+
+        // Метод для вычисления максимального числа
+        static int FindMax(params int[] numbers)
+        {
+            int max = numbers[0];
+
+            foreach (int number in numbers)
+
+                if (number > max)
+                    max = number;
+
+            return max;
+        }
+    }
+}
